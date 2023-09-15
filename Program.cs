7b91c6049590bf2aee00ebc9384eedfae4b6aecf
AdeyBoy123 <groveraden@gmail.com>
2023-09-15 16:06:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeTags
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the tag: ");
+            string tag = Console.ReadLine();
+            Console.WriteLine("Enter the word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(makeTags(tag, word));
+            Console.ReadLine();
+        }
+
+        static string makeTags(string tag, string word)
+        {
+            return "<" + tag + ">" + word + "</" + tag + ">";
+        }
+    }
+}
